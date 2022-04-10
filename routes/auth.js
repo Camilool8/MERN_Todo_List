fc3f0086a93787141c5dc30154ead5c3014ae135
Camilool8 <JCJG0001@CE.PUCMM.EDU.DO>
2022-04-10 11:44:44 -0400

    All API endpoints finished

--- routes/auth.js
+++ routes/auth.js
@@ -9,122 +9,150 @@ const requiresAuth = require("../middleware/permissions");
 // @route   GET api/auth/test
 // @desc    Tests auth route
 // @access  Public
-        router.get("/test", (req, res) => {
-            res.send("Test route");
-        });
+router.get("/test", (req, res) => {
+    res.send("Test route");
+});
 
 // @route   POST api/auth/register
 // @desc    Register user
 // @access  Public
 router.post("/register", async(req, res) => {
-    try {
+try {
 
 
-        // Validate errors in the user input fields
-        const { errors, isValid } = validateRegisterInput(req.body);
+    // Validate errors in the user input fields
+    const { errors, isValid } = validateRegisterInput(req.body);
 
-        if (!isValid) {
-            return res.status(400).json(errors);
-        }
-
-
-        // Check if user already exists
-        const existingEmail = await User.findOne({ 
-            email: new RegExp("^" + req.body.email + "$","i") 
-        });
-
-        if (existingEmail) {
-            return res.status(400).json({
-                errors: [{ msg: "Email already exists" }]
-            });
-        }
-
-
-        // encrypt the password of user
-        const hashedPassword = await bcrypt.hash(req.body.password, 12);
-
-        // Creates a new user
-        const newUser = new User({
-            email: req.body.email,
-            password: hashedPassword,
-            name: req.body.name,
-        });
-
-        // Saves user into database
-    const savedUser = await newUser.save();
-    const userToReturn = {...savedUser._doc};
-    delete userToReturn.password;
-
-        // return new user object
-    return res.json(userToReturn);
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send("Server Error");
+    if (!isValid) {
+        return res.status(400).json(errors);
     }
+
+
+    // Check if user already exists
+    const existingEmail = await User.findOne({ 
+        email: new RegExp("^" + req.body.email + "$","i") 
+    });
+
+    if (existingEmail) {
+        return res.status(400).json({
+            errors: [{ msg: "Email already exists" }]
+        });
+    }
+
+
+    // encrypt the password of user
+    const hashedPassword = await bcrypt.hash(req.body.password, 12);
+
+    // Creates a new user
+    const newUser = new User({
+        email: req.body.email,
+        password: hashedPassword,
+        name: req.body.name,
+    });
+
+    // Saves user into database
+const savedUser = await newUser.save();
+
+const payload = { userId: savedUser._id };
+
+const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "7d" });
+
+res.cookie("access-token", token, {
+    expires: new Date(Date.now() + 7 * 24 * 3600 * 1000),
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production"
+});
+
+const userToReturn = {...savedUser._doc};
+delete userToReturn.password;
+
+    // return new user object
+return res.json(userToReturn);
+} catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+}
 });
 
 // @route   POST api/auth/login
 // @desc    Login user and give token
 // @access  Public
 
-    router.post("/login", async(req, res) => {
-        try {
+router.post("/login", async(req, res) => {
+    try {
 
-            // Check if user exists
-            const user = await User.findOne({ 
-                email: new RegExp("^" + req.body.email + "$","i") 
+        // Check if user exists
+        const user = await User.findOne({ 
+            email: new RegExp("^" + req.body.email + "$","i") 
+        });
+
+        if (!user) {
+            return res.status(400).json({
+                errors: [{ msg: "Oops! There is an issue with your credentials..." }]
             });
-
-            if (!user) {
-                return res.status(400).json({
-                    errors: [{ msg: "Oops! There is an issue with your credentials..." }]
-                });
-            }
-
-            // Check password
-            const isMatch = await bcrypt.compare(req.body.password, user.password);
-
-            if (!isMatch) {
-                return res.status(400).json({
-                    errors: [{ msg: "Oops! There is an issue with your credentials..." }]
-                });
-            }
-
-            // Return jsonwebtoken
-            const payload = { userId: user._id };
-            const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "7d" });
-
-            res.cookie("access-token", token, {
-                expires: new Date(Date.now() + 7 * 24 * 3600 * 1000),
-                httpOnly: true,
-                secure: process.env.NODE_ENV === "production"
-            });
-
-            const userToReturn = {...user._doc};
-            delete userToReturn.password;
-
-            return res.json({
-                token: token,
-                user: userToReturn
-            });
-
-         
-        } catch (err) {
-            console.error(err);
-            return res.status(500).send(err.message);
         }
-    });
+
+        // Check password
+        const isMatch = await bcrypt.compare(req.body.password, user.password);
+
+        if (!isMatch) {
+            return res.status(400).json({
+                errors: [{ msg: "Oops! There is an issue with your credentials..." }]
+            });
+        }
+
+        // Return jsonwebtoken
+        const payload = { userId: user._id };
+        const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "7d" });
+
+        res.cookie("access-token", token, {
+            expires: new Date(Date.now() + 7 * 24 * 3600 * 1000),
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production"
+        });
+
+        const userToReturn = {...user._doc};
+        delete userToReturn.password;
+
+        return res.json({
+            token: token,
+            user: userToReturn
+        });
+
+        
+    } catch (err) {
+        console.error(err);
+        return res.status(500).send(err.message);
+    }
+});
 
 // @route   GET api/auth/current
 // @desc    Return current user authorized
 // @access  Private
 
-    router.get("/current",  requiresAuth, (req, res) => {
-        if(!req.user) {
-            return res.status(401).send("Unauthorized");
-        }
-        return res.json(req.user);
-    });
+router.get("/current",  requiresAuth, (req, res) => {
+    if(!req.user) {
+        return res.status(401).send("Unauthorized");
+    }
+    return res.json(req.user);
+});
+
+// @route   PUT api/auth/logout
+// @desc    Logout user and clear token
+// @access  Private
+
+
+router.put("/logout", requiresAuth, async(req, res) => {
+    try {
+        res.clearCookie("access-token");
+        return res.json({ msg: "Logged out" });
+    }
+    catch (err) {
+        console.error(err.message);
+        return res.status(500).send(err.message);
+    }
+});
+
 
 
 
--- routes/todos.js
+++ routes/todos.js
@@ -75,4 +75,153 @@ router.get("/current", requiresAuth, async (req, res) => {
 }
 );
 
+// @route PUT /api/todos/:toDoId/complete
+// @desc Mark ToDo as complete
+// @access Private
+
+router.put("/:toDoId/complete", requiresAuth, async (req, res) => {
+    try {
+    
+        const toDo = await ToDo.findOne({
+            user: req.user._id,
+            _id: req.params.toDoId,
+        });
+
+        if (!toDo) {
+            return res.status(404).json({error: "ToDo not found"});
+        }
+
+        if (toDo.complete) {
+            return res.status(400).json({error: "ToDo already completed"});
+        }
+
+        const udpdatedToDo = await ToDo.findOneAndUpdate({
+            user: req.user._id,
+            _id: req.params.toDoId,
+        }, {
+            complete: true,
+            completedAt: new Date(),
+        }, {
+            new: true,
+        });
+
+        return res.json(udpdatedToDo);
+
+
+
+    } catch (err) { 
+        console.error(err);
+
+        return res.status(500).send(err.message);
+    }
+});
+
+
+// @route PUT /api/todos/:toDoId/incomplete
+// @desc Mark ToDo as incomplete
+// @access Private
+
+router.put("/:toDoId/incomplete", requiresAuth, async (req, res) => {
+    try {
+        const toDo = await ToDo.findOne({
+            user: req.user._id,
+            _id: req.params.toDoId,
+        });
+
+        if (!toDo) {
+            return res.status(404).json({error: "ToDo not found"});
+        }
+
+        if (!toDo.complete) {
+            return res.status(400).json({error: "ToDo is already incomplete"});
+        }
+
+        const udpdatedToDo = await ToDo.findOneAndUpdate({
+            user: req.user._id,
+            _id: req.params.toDoId,
+        }, {
+            complete: false,
+            completedAt: null,
+        }, {
+            new: true,
+        });
+
+        return res.json(udpdatedToDo);
+    }
+    catch (err) {
+        console.error(err);
+        return res.status(500).send(err.message);
+    }
+});
+
+// @route PUT /api/todos/:toDoId
+// @desc Updating a toDo
+// @access Private
+
+router.put("/:toDoId", requiresAuth, async (req, res) => {
+    try{
+        const toDo = await ToDo.findOne({
+            user: req.user._id,
+            _id: req.params.toDoId,
+        });
+
+        if (!toDo) {
+            return res.status(404).json({error: "ToDo not found"});
+        }
+
+        const { errors, isValid } = validateToDoInput(req.body);
+
+        if (!isValid) {
+            return res.status(400).json(errors);
+        }
+
+        const updatedToDo = await ToDo.findOneAndUpdate({
+            user: req.user._id,
+            _id: req.params.toDoId,
+        }, {
+            content: req.body.content,
+        }, {
+            new: true,
+        });
+
+        return res.json(updatedToDo);
+    }
+    catch (err) {
+        console.error(err);
+        return res.status(500).send(err.message);
+    }
+})
+
+// @route DELETE /api/todos/:toDoId
+// @desc Delete a toDo
+// @access Private
+
+router.delete("/:toDoId", requiresAuth, async (req, res) => {
+    try {
+        const toDo = await ToDo.findOne({
+            user: req.user._id,
+            _id: req.params.toDoId,
+        });
+
+        if (!toDo) {
+            return res.status(404).json({error: "ToDo not found"});
+        }
+
+        await ToDo.findOneAndRemove({
+            user: req.user._id,
+            _id: req.params.toDoId,
+        });
+
+        return res.json({success: true});
+        
+    } catch (err) {
+        console.error(err);
+        return res.status(500).send(err.message);
+    }
+});
+
+
+
+
+
 module.exports = router;
